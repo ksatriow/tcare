920d15b09618c572c1abec6b37083c2cd0ead07f
NubScr1pt <kukuhsatriowibowo@gmail.com>
2024-07-26 09:34:05 +0700

    implement redis

--- UserService/main.go
+++ UserService/main.go
@@ -3,16 +3,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"github.com/go-redis/redis/v8"
 )
 
+var ctx = context.Background()
+
 func main() {
-	http.HandleFunc("/user", getUser)
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		err := rdb.Set(ctx, "user", "UserService", 0).Err()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		val, err := rdb.Get(ctx, "user").Result()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintln(w, val)
+	})
+
 	fmt.Println("UserService is running on :8081")
 	http.ListenAndServe(":8081", nil)
-}
-
-func getUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "User Service")
 }
